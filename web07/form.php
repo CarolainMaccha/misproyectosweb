7a9c59a883dcb361cc711c80f6ed83009f51b68b
CarolainMaccha <Carolinegiselle26@hotmail.com>
2021-11-12 14:06:16 +0100

    Web07

---
+++ web07/form.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Mi primer form procesado</title>
+  </head>
+  <body style="margin-left:40px;">
+    <h1>Mi primer form procesado</h1>
+    <?php
+      echo "Hola mundo!";
+    ?>
+    <br><br>
+    <form class="" action="procesar.php" method="post">
+
+      <label for="nombre">NOMBRE:</label>&nbsp;&nbsp;
+      <input type="text" name="nombre" value="">
+      <br><br>
+      <label for="apodo">APODO</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="text" name="apodo" value="">
+      <br><br>
+      <label for="">APELLIDO</label>&nbsp;
+      <input type="text" name="apellido" value="">
+      <br><br>
+      <label for="fechanacimiento">FECHA DE NACIMIENTO</label>&nbsp;
+      <input type="date" name="fechanacimiento" value="">
+      <br><br>
+      <label for="color">COLOR</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <input type="color" name="colorfavorito" value="">
+      <br><br>
+      <label for="nota">¿Qué nota cree que sacá en Aplicaciones Web?</label>&nbsp;
+      <input type="range" name="nota" value="">
+      <br><br>
+      <label for="zodiaco">Signo del zodiaco</label>&nbsp;&nbsp;
+      <select name="Zodiaco">
+        <option value="Cáncer">Cáncer</option>
+        <option value="Acuario">Acuario</option>
+        <option value="Piscis">Piscis</option>
+        <option value="Sagitario" selected>Sagitario</option>
+        <option value="Capricornio">Capricornio</option>
+        <option value="Libra">Libra</option>
+        <option value="leo">Leo</option>
+        <option value="Tauro" selected>Tauro</option>
+      </select> <br><br>
+      <input type="text" name="prueba" value="" placeholder="Información"><br><br>
+      <input type="submit" name="" value="Enviar">
+
+    </form>
+  </body>
+</html>
---
+++ web07/listado.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Listado de Información</h1>
+    <button onclick="location.href='form.php'">Agregar Nuevo Usuario</button>
+    <br><br>
+    <?php
+    $fichero = fopen("datos.txt", "r");
+    while($data = fgetcsv($fichero, 1000, " ")) {
+        echo $data[0] . " " . $data[1] . " ";
+        echo $data[2] . " " . $data[3] . " ";
+        echo $data[4] . " " . $data[5] . " ";
+        echo $data[6] . "<br>";
+    }
+    fclose($fichero);
+    ?>
+
+  </body>
+</html>
